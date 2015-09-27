85dc4ff574342c59878c4b5437041beb6fe19e30
jin <jinmin.hu@altran.com>
2015-09-27 22:49:21 +0200

    refactor: move oauth to user

--- src/chimera/reasoning/handler.clj
+++ src/chimera/reasoning/handler.clj
@@ -2,13 +2,9 @@
   (:require
     [compojure.api.sweet :refer :all]
     ;[compojure.core :refer :all]
-    [compojure.handler :as handler :refer [site]]
       [ring.util.http-response :refer :all]
               [chimera.reasoning.example :refer :all]
               [chimera.reasoning.measurement :refer :all]
-    [cemerick.friend :as friend]
-    [friend-oauth2.workflow :as oauth2]
-    [friend-oauth2.util :refer [format-config-uri]]
     [chimera.reasoning.user :refer :all]
               [chimera.reasoning.device :refer :all]
 
@@ -20,13 +16,7 @@
         measurement-routes
         device-routes
         example-routes
-        (handler/site
-          (friend/authenticate
-            user-routes
-            {:allow-anon? true
-             :workflows   [(oauth2/workflow
-                             {:client-config client-config
-                              :uri-config    uri-config
-                              :credential-fn credential-fn})]}))
+        user-routes
+
         )
 
--- src/chimera/reasoning/user.clj
+++ src/chimera/reasoning/user.clj
@@ -2,7 +2,10 @@
   (:require [schema.core :as s]
             [compojure.api.sweet :refer :all]
             [ring.util.http-response :refer :all]
+
+            [compojure.handler :as handler :refer [site]]
             [cemerick.friend :as friend]
+            [friend-oauth2.workflow :as oauth2]
             [friend-oauth2.util :refer [format-config-uri]]
             [ring.swagger.schema :refer [coerce!]]
             )
@@ -87,24 +90,33 @@
                                 :redirect_uri  (format-config-uri client-config)}}})
 
 (defroutes* user-routes
-            (context*
-              "/user" []
-              :tags ["user"]
-              (GET* "/" request "open.")
-              (GET* "/status" request
-             (let [count (:count (:session request) 0)
-                   session (assoc (:session request) :count (inc count))]
-               (-> (ring.util.response/response
-                     (str "<p>We've hit the session page " (:count session)
-                          " times.</p><p>The current session: " session "</p>"))
-                   (assoc :session session))))
-              (GET* "/authlink" request
-             (friend/authorize #{::user} "Authorized page."))
-              (GET* "/authlink2" request
-             (friend/authorize #{::user} "Authorized page 2."))
-              (GET* "/admin" request
-             (friend/authorize #{::admin} "Only admins can see this page."))
-              (friend/logout (ANY* "/logout" request (ring.util.response/redirect "/"))))
+            (handler/site
+              (friend/authenticate
+                (context*
+                  "/user" []
+                  :tags ["user"]
+                  (GET* "/" request "open.")
+                  (GET* "/status" request
+                        (let [count (:count (:session request) 0)
+                              session (assoc (:session request) :count (inc count))]
+                          (-> (ring.util.response/response
+                                (str "<p>We've hit the session page " (:count session)
+                                     " times.</p><p>The current session: " session "</p>"))
+                              (assoc :session session))))
+                  (GET* "/authlink" request
+                        (friend/authorize #{::user} "Authorized page."))
+                  (GET* "/authlink2" request
+                        (friend/authorize #{::user} "Authorized page 2."))
+                  (GET* "/admin" request
+                        (friend/authorize #{::admin} "Only admins can see this page."))
+                  (friend/logout (ANY* "/logout" request (ring.util.response/redirect "/"))))
+                {:allow-anon? true
+                 :workflows   [(oauth2/workflow
+                                 {:client-config client-config
+                                  :uri-config    uri-config
+                                  :credential-fn credential-fn})]}))
+
+
             )
 
 ;;(def app
